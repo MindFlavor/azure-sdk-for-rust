4a95aadafaf924d1cae5b72ac0d92d8b25158886
Francesco Cogno <francesco.cogno@outlook.com>
2021-07-13 09:59:02 +0200

    R -> C generic

--- sdk/core/src/policies/mod.rs
+++ sdk/core/src/policies/mod.rs
@@ -17,6 +17,7 @@ pub type PolicyResult<T> = Result<T, Box<dyn Error + Send + Sync>>;
 /// Policies can then inspect the response, potentially signaling failure.
 /// The only runtime enforced check is that the last policy must be a Transport policy. It's up to
 /// the implementer to call the following policy.
+/// The `C` generic represents the *contents* of the AuthorizationPolicy specific of this pipeline.
 #[async_trait::async_trait]
 pub trait Policy<C>: Send + Sync + std::fmt::Debug
 where
